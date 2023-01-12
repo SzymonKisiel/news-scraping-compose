404f8f6dc3fceca4153c331390456777c7719104
skisiel <szymon.kisiel99@gmail.com>
2023-01-12 17:38:31 +0100

    Updated database init file

--- sql_init/01_init_db.sql
+++ sql_init/01_init_db.sql
@@ -84,9 +84,9 @@ VALUES
 	(1, 'neutral'),
 	(2, 'positive');
 
--- init procedures
+  DELIMITER $$
 
-DELIMITER $$
+-- init procedure AddSearchTerm
 
 DROP PROCEDURE IF EXISTS AddSearchTerm $$
 
@@ -123,6 +123,53 @@ BEGIN
 		);
 		IF @client_search_term_count > 0 THEN
 			SET @msg = CONCAT('Client with name ', in_client_name, ' already has term ', in_search_term);
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @msg;
+		END IF;
+	END IF;
+
+	INSERT INTO news_scraping_db.client_search_term
+		(client_id, search_term_id)
+	VALUES
+		(@client_id, @search_term_id);
+END $$
+
+-- init procedure AddSearchTermById
+
+DROP PROCEDURE IF EXISTS AddSearchTermById $$
+
+CREATE PROCEDURE AddSearchTermById(IN in_client_id VARCHAR(100), IN in_search_term VARCHAR(200))
+BEGIN
+	SET @client_id := (
+		SELECT c.id
+		FROM news_scraping_db.client c
+		WHERE c.id = in_client_id
+	);
+	SET @search_term_id := (
+		SELECT st.id
+		FROM news_scraping_db.search_term AS st
+		WHERE st.term = in_search_term
+	);	
+
+	IF @client_id IS NULL THEN
+		SET @msg = CONCAT('Client with id ', in_client_id, ' does not exist');
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @msg;
+	END IF;
+	
+	IF @search_term_id IS NULL THEN
+		INSERT INTO news_scraping_db.search_term
+			(term)
+		VALUES
+			(in_search_term);
+		SET @search_term_id := LAST_INSERT_ID();
+	ELSE
+		SET @client_search_term_count := (
+			SELECT COUNT(*)
+			FROM news_scraping_db.client_search_term AS cst
+			WHERE cst.client_id = @client_id AND
+				cst.search_term_id = @search_term_id
+		);
+		IF @client_search_term_count > 0 THEN
+			SET @msg = CONCAT('Client with id ', in_client_id, ' already has term ', in_search_term);
 			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @msg;
 		END IF;
 	END IF;
